2e1dcb07860ed48f8e793278e1d5c3a5f85aff8a
Kirill Yuldashev <kirill.yuldashev@mail.ru>
2018-01-07 00:24:35 +0500

    smthing works

--- ftp.py
+++ ftp.py
@@ -1,10 +1,15 @@
 import sys
-import getpass
 import socket
+import re
+import getpass
+from random import randint
 
 
+PASV = False
+ADDR_PATTERN = re.compile(r'(\d+),(\d+),(\d+),(\d+),(\d+),(\d+)')
 CR_LF = '\r\n'
 MAX_LINE = 8192
+MAX_WIN = 65535
 FTP_PORT = 21
 
 
@@ -16,17 +21,17 @@ def create_connection(host='', port=FTP_PORT, timeout=2):
         raise ConnectionError(e)
 
 
-def send_cmd(sock, cmd, arg=''):
+def send_cmd(control, cmd, arg=''):
     """Sends command to server through control socket."""
     if not arg:
-        sock.sendall(construct_msg(cmd, arg))
+        control.sendall(construct_msg(cmd, arg))
     else:
-        sock.sendall(construct_msg(cmd, ' ', arg))
+        control.sendall(construct_msg(cmd, ' ', arg))
 
 
-def get_reply(sock):
+def get_reply(control):
     """Gets server's reply, decode and returns it."""
-    return receive_bytes(sock).decode()
+    return receive_bytes(control, MAX_LINE).decode()
 
 
 def construct_msg(*args):
@@ -36,12 +41,12 @@ def construct_msg(*args):
     return ''.join(msg).encode()
 
 
-def receive_bytes(sock):
+def receive_bytes(connection, buf_size):
     """Receives bytes data from server while we can."""
     answer = []
     while True:
         try:
-            raw = sock.recv(MAX_LINE)
+            raw = connection.recv(buf_size)
             if not raw:
                 break
             answer.append(raw)
@@ -50,35 +55,93 @@ def receive_bytes(sock):
     return b''.join(answer)
 
 
-def print_reply(sock):
+def print_reply(control):
     """Prints server's reply."""
-    reply = get_reply(sock).replace('\r\n', '')
-    print(">", reply)
+    reply = get_reply(control).replace('\r\n', '')
+    print(reply)
 
 
-def user(sock):
+def user(control):
     """USER command."""
-    username = input("> USER: ")
-    send_cmd(sock, "user", username)
-    print_reply(sock)
+    username = input("USER: ")
+    send_cmd(control, "user", username)
+    reply = get_reply(control).replace('\r\n', '')
+    print(reply)
+    if reply[0] == '5':
+        print("Login error.")
+    else:
+        pass_(control)
 
 
-def pass_(sock):
+def pass_(control):
     """PASS command."""
-    password = getpass.getpass("> PASS: ")
-    send_cmd(sock, "pass", password)
-    print_reply(sock)
+    password = getpass.getpass("PASS: ")
+    send_cmd(control, "pass", password)
+    print_reply(control)
 
 
-if __name__ == "__main__":
-    sock = create_connection('speedtest.tele2.net')
-    print_reply(sock)
-    send_cmd(sock, 'help')
-    print_reply(sock)
-    user(sock)
-    pass_(sock)
-    send_cmd(sock, 'help')
-    """
-        while True:
-        cmd = input(">>>")
-    """
+def pasv(control):  # <--- PASSIVE MODE
+    """PASV command. Initiate data connection.."""
+    send_cmd(control, "pasv")
+    reply = get_reply(control)
+    print(reply.replace('\r\n', ''))
+    address = re.findall(ADDR_PATTERN, reply)[0]
+    host, port = '.'.join(address[:4]), int(address[4])*256 + int(address[5])
+    data_conn = socket.create_connection((host, port))
+    return data_conn
+
+
+def port_(control):  # <--- ACTIVE MODE
+    """PORT command. Server initiates connection."""
+    conn_handler = socket.socket()
+    conn_handler.bind((control.getsockname()[0], randint(1024, 65535)))
+    conn_handler.listen(1)
+    host, port = conn_handler.getsockname()
+    address = f"{host.replace('.', ',')},{port // 256},{port % 256}"
+    send_cmd(control, "port", address)
+    print_reply(control)
+    return conn_handler
+
+
+def get_data(control, cmd):
+    """Gets data by command depends on active/passive mode."""
+    if not PASV:
+        temp = port_(control)
+        send_cmd(control, cmd)
+        print(control.recv(MAX_LINE).decode().replace('\r\n', ''))  # msg before data transport
+        data_conn, addr = temp.accept()
+    else:
+        data_conn = pasv(control)
+        send_cmd(control, cmd)
+        print(control.recv(MAX_LINE).decode().replace('\r\n', ''))  # msg before data transport
+    return receive_bytes(data_conn, MAX_WIN).decode()
+
+
+def list_(control):
+    """LIST command."""
+    data = get_data(control, "list")
+    print(data[:-1])  # remove last \r\n
+    print_reply(control)
+
+
+def cwd(control, path):
+    """CWD command."""
+    if path == "..":
+        send_cmd(control, "cdup")
+    else:
+        send_cmd(control, "cwd", path)
+    print_reply(control)
+
+
+def pwd(control):
+    """PWD command."""
+    send_cmd(control, "pwd")
+    print_reply(control)
+
+
+def quit_(control):
+    """QUIT command."""
+    send_cmd(control, "quit")
+    print_reply(control)
+    sys.exit()
+
--- main.py
+++ main.py
@@ -1,12 +1,61 @@
 import argparse
-import sys
-import getpass
+
+
+try:
+    from ftp import *
+except ImportError as e:
+    print(e, file=sys.stderr)
+    sys.exit()
+
+
+def help_(cmd=None):
+    """Help (commands)."""
+    if cmd is None:
+        print("Help about all...")
+    else:
+        print("Help[cmd]... Here will be dict with help...")
 
 
 def parse_args():
-    parser = argparse.ArgumentParser("")
-    parser.add_argument()
+    parser = argparse.ArgumentParser(description="Simple ftp client.")
+    parser.add_argument("host", help="Host to connect.")
+    parser.add_argument('port', help='Port to connect.', nargs='?', type=int, default=21)
+    parser.add_argument("--passive", '-p', help="Use passive mode or not.", action="store_true", dest='passive')
+    return parser.parse_args()
+
+
+COMMANDS = {"ls": list_, "user": user, "pass": pass_, "cd": cwd, "pwd": pwd, "quit": quit_, "?": help_}
+
+
+def main():
+    args = parse_args()
+    global PASV
+    PASV = True if args.passive else False
+    try:
+        control = create_connection(args.host, args.port)
+        print(f"Connecting to {args.host}...")
+        print_reply(control)
+
+        user(control)
+
+        while True:
+            input_ = input("ftp> ").split(' ')
+            cmd = input_[0].lower()
+            arg = input_[1] if len(input_) > 1 else ''
+            try:
+                COMMANDS[cmd](control, arg) if arg != '' else COMMANDS[cmd](control)
+            except KeyError:
+                print(f"Unsupported command {cmd}.")
+            except Exception as exception:
+                print(exception, file=sys.stderr)
+
+    except ConnectionError as exception:
+        print(exception, file=sys.stderr)
+        sys.exit()
 
 
 if __name__ == "__main__":
-    pass
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()
